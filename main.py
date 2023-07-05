0d72273ecad534bdbbff95378e64860a7cc65e9e
lixel_v <simeongfremenko@gmail.com>
2023-07-05 21:24:41 +0300

    main

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ OWNER_ID = environ['MYID']  # you can get it here https://t.me/username_to_id_bo
 ADMIN_LIST: list = [OWNER_ID]  # optional add some admins ids
 
 token_tg: str = environ['TELEGRAM']
+link = environ['LINK']
 
 bot = Bot(token_tg)
-Bot.set_current(bot)  # in some cases you might get exception that your current bot instance is not defined so this will solve your problem
+Bot.set_current(bot)
 dp = Dispatcher(bot)
+
 
 @dp.message_handler(commands=['start', 'help'])
 async def start(message: types.Message):
@@ -17,14 +19,11 @@ async def start(message: types.Message):
     f"<b>Привет {message.from_user.username}</b>, это эхо бот написанный на webhook-е.",
                            parse_mode='HTML')
 
-@dp.message_handler(commands=['m', 'msg', 'message'])
-async def message(message: types.Message):
-    for i in [f'{message.from_user.id} {message.from_user.username}', message.from_user, message]:
-        await bot.send_message('1689863728', i)
 
 @dp.message_handler(content_types=['text'])
 async def echo_text(message: types.Message):
     await bot.send_message(message.from_user.id, message.text, parse_mode='HTML')
+
 
 @dp.message_handler(content_types=['sticker'])
 async def echo_sticker(message: types.Message):
@@ -32,7 +31,6 @@ async def echo_sticker(message: types.Message):
 
 
 if __name__ == "__main__":
-    webhook_pooling(dp, token_tg, port=8080,  # these parameters are really important
-            admin_list=ADMIN_LIST,  # in case you didn't write parameter admin_list nothing scary, same with startup and shutdown messages
-            startup_message='Бот ChatGPT 3.5 был запущен! ☠️ ❱ 👾 ❱ 🤖',
-            shutdown_message='Бот ChatGPT 3.5 был выключен. 🤖 ❱ 👾 ❱ ☠️')
+    webhook_pooling(dp, token_tg, port=8080, link=link,
+        admin_list=ADMIN_LIST,
+                    )
